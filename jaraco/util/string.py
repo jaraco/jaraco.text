9f6885a45f0c524d1329eb3992ed4aa32af1be3a
Jason R. Coombs <jaraco@jaraco.com>
2013-05-16 13:43:52 -0400

    Added simple_html_strip

--- jaraco/util/string.py
+++ jaraco/util/string.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import, unicode_literals, print_function
 
 import sys
 import re
@@ -214,3 +214,29 @@ def words(identifier):
 	"""
 	pattern = re.compile('([A-Z]?[a-z]+)|([A-Z]+(?![a-z]))')
 	return WordSet(match.group(0) for match in pattern.finditer(identifier))
+
+
+def simple_html_strip(s):
+	r"""
+	Remove HTML from the string `s`.
+
+	>>> str(simple_html_strip(''))
+	''
+
+	>>> print(simple_html_strip('A <bold>stormy</bold> day in paradise'))
+	A stormy day in paradise
+
+	>>> print(simple_html_strip('Somebody <!-- do not --> tell the truth.'))
+	Somebody  tell the truth.
+
+	>>> print(simple_html_strip('What about<br/>\nmultiple lines?'))
+	What about
+	multiple lines?
+	"""
+	html_stripper = re.compile('(<!--.*?-->)|(<[^>]*>)|([^<]+)', re.DOTALL)
+	texts = (
+		match.group(3) or ''
+		for match
+		in html_stripper.finditer(s)
+	)
+	return ''.join(texts)
