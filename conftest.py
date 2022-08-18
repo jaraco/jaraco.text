d6ce82d6a504075989a9dd79ecaae63105f8ba39
Jason R. Coombs <jaraco@jaraco.com>
2022-08-18 16:28:14 -0400

    Wrap tmp_path in a pathlib2.Path for compatibility on older Pythons. Fixes #10.

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import sys
+
+import pytest
+
+
+if sys.version_info < (3, 10):
+    import pathlib2 as pathlib  # pragma: nocover
+else:
+    import pathlib  # pragma: nocover
+
+
+@pytest.fixture
+def tmp_path(tmp_path):
+    """
+    Override tmp_path to wrap in a more modern interface.
+    """
+    return pathlib.Path(tmp_path)
